4f0a8e96c891e1355ec124d2c381d2716fdd466f
Mamoudou12 <bmamoudou463@gmail.com>
2024-10-01 12:33:18 +0000

    modifiying recipeModel file

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -1,19 +1,21 @@
 import db from '../config/db.js';
 
 class Recipe {
-  // Récupérer toutes les recettes
   static async getAllRecipes() {
     const [results] = await db.query('SELECT * FROM recipes');
     return results;
   }
 
-  // Récupérer une recette par ID
   static async getRecipeById(id) {
     const [results] = await db.query('SELECT * FROM recipes WHERE id = ?', [id]);
     return results.length > 0 ? results[0] : null;
   }
 
-  // Créer une nouvelle recette
+  static async getRecipeByTitle(title) {
+    const [results] = await db.query('SELECT * FROM recipes WHERE title = ?', [title]);
+    return results.length > 0 ? results[0] : null;
+  }
+
   static async createRecipe(title, ingredients, type) {
     const [result] = await db.query(
       'INSERT INTO recipes (title, ingredients, type) VALUES (?, ?, ?)',
@@ -22,7 +24,6 @@ class Recipe {
     return result.insertId;
   }
 
-  // Mettre à jour une recette existante
   static async updateRecipe(id, title, ingredients, type) {
     const [result] = await db.query(
       'UPDATE recipes SET title = ?, ingredients = ?, type = ? WHERE id = ?',
@@ -31,7 +32,6 @@ class Recipe {
     return result.affectedRows;
   }
 
-  // Supprimer une recette
   static async deleteRecipe(id) {
     const [result] = await db.query('DELETE FROM recipes WHERE id = ?', [id]);
     return result.affectedRows;
